06303f6cfd8c5bba5f5e46d37ec8b140703aed41
noeyah2 <hy0708@cau.ac.kr>
2025-08-10 21:57:16 +0900

    feat(auth): OrderPlatformMsaAuthApplication redis관련 코드 추가
    redisRepository 활성화
    스케줄링 기능 활성화

--- src/main/java/app/OrderPlatformMsaAuthApplication.java
+++ src/main/java/app/OrderPlatformMsaAuthApplication.java
@@ -2,8 +2,12 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableRedisRepositories
 public class OrderPlatformMsaAuthApplication {
 
 	public static void main(String[] args) {
